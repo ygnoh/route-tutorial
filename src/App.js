7b1057776b50a95a8f297499b445be4249434e51
yonggoonoh <nohyg90@gmail.com>
2017-10-08 00:30:20 +0900

    Switch component를 통한 404 page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Header from "./components/Header";
 import Home from "./routes/Home";
@@ -8,18 +8,22 @@ import Posts from "./routes/Posts";
 import Search from "./routes/Search";
 import Login from "./routes/Login";
 import MyPage from "./routes/MyPage";
+import NoMatch from "./routes/NoMatch";
 
 const App = () => {
 	return (
 		<Router>
 			<div>
 				<Header />
-				<Route exact path="/" component={Home} />
-				<Route path="/about/:username" component={About} />
-				<Route path="/posts" component={Posts} />
-				<Route path="/search" component={Search} />
-				<Route path="/login" component={Login} />
-				<Route path="/me" component={MyPage} />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/about/:username" component={About} />
+					<Route path="/posts" component={Posts} />
+					<Route path="/search" component={Search} />
+					<Route path="/login" component={Login} />
+					<Route path="/me" component={MyPage} />
+					<Route component={NoMatch} />
+				</Switch>
 			</div>
 		</Router>
 	);
