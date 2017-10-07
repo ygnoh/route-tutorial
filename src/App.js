bbf520dfce66f5f8d013b0816ed7705ee5404a40
yonggoonoh <nohyg90@gmail.com>
2017-10-07 23:46:24 +0900

    home과 about 페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 const App = () => {
 	return (
-		<div>
-			App
-		</div>
+		<Router>
+			<div>
+				<Route exact path="/" component={Home} />
+				<Route path="/about" component={About} />
+			</div>
+		</Router>
 	);
 };
 
