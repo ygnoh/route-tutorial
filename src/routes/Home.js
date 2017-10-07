174ee7139417118ef508adffb807de89b1f57f48
yonggoonoh <nohyg90@gmail.com>
2017-10-08 00:22:05 +0900

    props.history.push를 이용한 redirect

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Home = () => {
+const Home = ({history}) => {
     return (
         <div>
             Home
+            <button onClick={()=>{history.push("/posts")}}>
+                goToPosts
+            </button>
         </div>
     );
 };
